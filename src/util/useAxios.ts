ee7f2ae1b12cb5e38733ad4d1aa91e14bc816923
Marius Modlich <marius@modlich.de>
2023-02-18 13:44:43 +0100

    fix(backend): switched backend to Vercel

--- src/util/useAxios.ts
+++ src/util/useAxios.ts
@@ -1,55 +1,55 @@
-import axios from "axios";
-import { headersType, methodType, urlType } from "../constants/BasicTypes";
+import axios from "axios"
+import { headersType, methodType, urlType } from "../constants/BasicTypes"
 
 // export const BACKEND_URL = "http://localhost:5000";
 // export const BACKEND_URL = "https://hackerlist-backend.vercel.app";
-export const BACKEND_URL = "https://api.hopers.io";
+export const BACKEND_URL = "https://hopers-backend-beige.vercel.app"
 // export const BACKEND_URL = "https://proxy-backend.vercel.app";
 
-const subQueryUrl = "https://api.subquery.network/sq/VenusDev0725/hoperswap";
+const subQueryUrl = "https://api.subquery.network/sq/VenusDev0725/hoperswap"
 
 const fetch = ({
 	method,
 	url,
 	headers,
-	data,
+	data
 }: {
-	method: methodType;
-	url: urlType;
-	headers?: any;
-	data?: any;
+	method: methodType
+	url: urlType
+	headers?: any
+	data?: any
 }) => {
 	return axios({
 		method,
 		url,
 		headers,
-		data,
-	});
-};
+		data
+	})
+}
 
 const getQuery = async ({
 	url,
 	method,
-	headers,
+	headers
 }: {
-	url: urlType;
-	method?: methodType;
-	headers?: headersType;
+	url: urlType
+	method?: methodType
+	headers?: headersType
 }) => {
 	try {
 		const result = await fetch({
 			url,
 			method: method || "get",
-			headers: { "content-type": "application/json" },
-		});
-		return result.data;
+			headers: { "content-type": "application/json" }
+		})
+		return result.data
 	} catch (err) {
-		console.error("axios error at", url, method, err);
-		return null;
+		console.error("axios error at", url, method, err)
+		return null
 	}
-};
+}
 
-export default getQuery;
+export default getQuery
 
 export const getDexStatus = async () => {
 	try {
@@ -65,16 +65,16 @@ export const getDexStatus = async () => {
           txNumber
         }
       }
-    }`;
+    }`
 		const {
 			data: {
 				data: {
-					tradingVolumeEntities: { nodes },
-				},
-			},
-		} = await axios.post(subQueryUrl, { query });
-		return nodes;
+					tradingVolumeEntities: { nodes }
+				}
+			}
+		} = await axios.post(subQueryUrl, { query })
+		return nodes
 	} catch (err) {
-		return [];
+		return []
 	}
-};
+}
