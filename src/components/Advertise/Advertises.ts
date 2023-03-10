454083f798ee8957e1ea82db9622448f6f4e3148
bleepingnoise <114673957+bleepingnoise@users.noreply.github.com>
2023-03-10 21:19:50 +0100

    Updated adversises

--- src/components/Advertise/Advertises.ts
+++ src/components/Advertise/Advertises.ts
@@ -1,57 +1,67 @@
 import { AdvertiseItemProps } from ".";
 
 export const Advertise1: AdvertiseItemProps[] = [
-	{
-		url: "/advertises-images/image_011.png",
-		description: 
-			"#ETHEREUM has officially landed on Hopers.io, paving the way for the future of #DeFi."
-	},
-	{
-		url: "/advertises-images/image_010.png",
-		description: 
-			"The first #IBC-compatible EVM-based chain, $EVMOS, is now available for the first time on $JUNO ☄️"
-	},
-	{
-		url: "/advertises-images/image_008.png",
-		description:
-			"We have just listed $MARS, the new L1 credit protocol on Cosmos🛸",
-	},
-	{
-		url: "/advertises-images/image_009.png",
-		description:
-			"Hopers is proud to announce the listing of $KUJI , the token of the sustainable #FinTech ecosystem TeamKujira",
-	},
+    {
+        url: "/advertises-images/image_012.png",
+        description:
+            "We're proud to announce that #BITCOIN  is joining our DEX for the first time ever powered by @axelarcore!",
+    },
+    {
+        url: "/advertises-images/image_011.png",
+        description:
+            "#ETHEREUM has officially landed on Hopers.io, paving the way for the future of #DeFi.",
+    },
+    {
+        url: "/advertises-images/image_010.png",
+        description:
+            "The first #IBC-compatible EVM-based chain, $EVMOS, is now available for the first time on $JUNO ☄️",
+    },
+    {
+        url: "/advertises-images/image_008.png",
+        description:
+            "We have just listed $MARS, the new L1 credit protocol on Cosmos🛸",
+    },
+    {
+        url: "/advertises-images/image_009.png",
+        description:
+            "Hopers is proud to announce the listing of $KUJI , the token of the sustainable #FinTech ecosystem TeamKujira",
+    },
 ];
 
 export const Advertise2: AdvertiseItemProps[] = [
-	{
-		url: "/advertises-images/image_011.png",
-		description: 
-			"#ETHEREUM has officially landed on Hopers.io, paving the way for the future of #DeFi."
-	},
-	{
-		url: "/advertises-images/image_010.png",
-		description: 
-			"The first #IBC-compatible EVM-based chain, $EVMOS, is now available for the first time on $JUNO ☄️"
-	},
-	{
-		url: "/advertises-images/image_008.png",
-		description:
-			"We have just listed $MARS, the new L1 credit protocol on Cosmos🛸",
-		style: {
-			description: {
-				color: "black",
-			},
-		},
-	},
-	{
-		url: "/advertises-images/image_009.png",
-		description:
-			"Hopers is proud to announce the listing of $KUJI , the token of the sustainable #FinTech ecosystem TeamKujira",
-		style: {
-			description: {
-				color: "black",
-			},
-		},
-	},
+    {
+        url: "/advertises-images/image_012.png",
+        description:
+            "We're proud to announce that #BITCOIN  is joining our DEX for the first time ever powered by @axelarcore!",
+    },
+    {
+        url: "/advertises-images/image_011.png",
+        description:
+            "#ETHEREUM has officially landed on Hopers.io, paving the way for the future of #DeFi.",
+    },
+    {
+        url: "/advertises-images/image_010.png",
+        description:
+            "The first #IBC-compatible EVM-based chain, $EVMOS, is now available for the first time on $JUNO ☄️",
+    },
+    {
+        url: "/advertises-images/image_008.png",
+        description:
+            "We have just listed $MARS, the new L1 credit protocol on Cosmos🛸",
+        style: {
+            description: {
+                color: "black",
+            },
+        },
+    },
+    {
+        url: "/advertises-images/image_009.png",
+        description:
+            "Hopers is proud to announce the listing of $KUJI , the token of the sustainable #FinTech ecosystem TeamKujira",
+        style: {
+            description: {
+                color: "black",
+            },
+        },
+    },
 ];
